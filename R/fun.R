f8ca34f4e3353e76b6b2ed20f9a710f05df853ac
mischko <mhem@mailbox.org>
2024-11-14 20:39:23 +0100

    add function description

--- R/fun.R
+++ R/fun.R
@@ -1,6 +1,15 @@
-# dummy function
-plotFun <- function(x) {
-    plot <- ggplot(x, aes(x = x, y = y)) +
+#' Plot Function
+#'
+#' This function creates a scatter plot using ggplot2.
+#'
+#' @param df The data frame containing the variables to be plotted.
+#' @param x The name of the variable to be plotted on the x-axis.
+#' @param y The name of the variable to be plotted on the y-axis.
+#' @return A ggplot object representing the scatter plot.
+#' @export
+plotFun <- function(df, x, y) {
+    plot <-
+        ggplot(df, aes(x = x, y = y)) +
         geom_point()
     return(plot)
 }
